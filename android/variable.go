2dca864fedb45f9fabc32e0633686f54a73a74c1
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with wave board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of wave variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"wave/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -124,6 +126,8 @@ type variableProperties struct {
 		Device_support_hwfde_perf struct {
 			Cflags []string
 		}
+		// include wave variables
+		Wave android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -242,6 +246,9 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
+
+	// include wave variables
+	Wave android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
